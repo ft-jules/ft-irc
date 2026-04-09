a216ea7d5853d11e22d8c2275ee4bc3c06f8465f
ft-jules <jpointil@student.42.fr>
2026-04-09 23:14:02 +0200

    Removed deleted files

--- CommandPing.cpp
+++
@@ -1,8 +0,0 @@
-#include "CommandPing.hpp"
-
-void CommandPing::execute(const std::string &args, Client *client)
-{
-	// std::cout << INVERSE_BG << BLUE << "PING args: " BOLD << args << RESET << std::endl;
-	
-	Reply::sendReply(client, "PONG :" + args);
-}
--- poll/PollEvent.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct PollEvent
-{
-	int fd;
-	bool readable;
-	bool error;
-};
